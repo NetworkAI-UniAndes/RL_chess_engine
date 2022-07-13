1376e8cb9fda74d68c109dbccb935e8bed585f7d
Nicolás Vergara <37335567+Switchfools@users.noreply.github.com>
2022-07-13 11:25:26 -0500

    10 games

--- src/models/data_scientist/train_transformer.py
+++ src/models/data_scientist/train_transformer.py
@@ -323,7 +323,7 @@ if __name__=="__main__":
             
             train_label = train_label.to(device)
             target_input =target_input.to(device)
-            target_input =torch.reshape(target_input,(target_input.size()[0],1))
+            target_input =torch.reshape(train_label,(target_input.size()[0],1))
             mask_target = target_input.to(device)
             mask_input = train_input.to(device)
             input_id = train_input.squeeze(1).to(device)
@@ -347,7 +347,7 @@ if __name__=="__main__":
 
                 val_label = val_label.to(device)
                 target_input =target_input.to(device)
-                target_input =torch.reshape( target_input,(target_input.size()[0],1))
+                target_input =torch.reshape( val_label,(target_input.size()[0],1))
                 mask_target = target_input.to(device)
                 mask = val_input.to(device)
                 input_id = val_input.squeeze(1).to(device)
