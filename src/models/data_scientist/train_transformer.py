95da1212d8029efec663cfce3cf29a6ac20b2028
Nicolás Vergara <37335567+Switchfools@users.noreply.github.com>
2022-07-16 18:16:53 -0500

    changing batch size

--- src/models/data_scientist/train_transformer.py
+++ src/models/data_scientist/train_transformer.py
@@ -306,7 +306,7 @@ if __name__=="__main__":
     train, val = Dataset(fens_train,legal_moves_train,target_moves_train,last_moves_train,
                         moves_dict),Dataset(fens_val,legal_moves_val,target_val,last_moves_val,moves_dict)
 
-    BATCH_SIZE=1000
+    BATCH_SIZE=500
     train_dataloader = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
     val_dataloader = torch.utils.data.DataLoader(val, batch_size=BATCH_SIZE)
 
